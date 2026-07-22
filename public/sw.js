9c81e81783cc8627f03558fd1b3263e01c4cc920
farmerintech <farmerintech@gmail.com>
2026-07-22 07:37:37 +0100

    for web

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE = "adhkar-v1";
+
+const ASSETS = ["/", "/index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    }),
+  );
+});
